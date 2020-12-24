741c775bb55407bc90e8e9135666cd194a42155e
ahmed elshiekh <ahmedel.sh50@gmail.com>
2020-12-24 15:17:24 -0800

    from server open register

---
+++ storage/framework/views/de2f5374cd2c9f80d43661c133e9e98410aa4694.php
@@ -0,0 +1,3 @@
+
+<img src="<?php echo e(asset('logo/logo.png')); ?>" style="width:10rem;height: 100%">
+<?php /**PATH C:\Websites\bussiness31\carton_v2\resources\views/vendor/jetstream/components/application-logo.blade.php ENDPATH**/ ?>
